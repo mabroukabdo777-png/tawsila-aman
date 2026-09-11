7a9ea1b076522725311752ac879a3143d22ce099
mabroukabdo777-png <mabroukabdo777@gmail.com>
2026-09-12 02:01:39 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,7 @@
-self.addEventListener('fetch', e=>{})
+const VERSION='v4-black-premium';
+self.addEventListener('install',e=>{self.skipWaiting()});
+self.addEventListener('activate',e=>{
+  e.waitUntil(clients.claim());
+  e.waitUntil(caches.keys().then(k=>Promise.all(k.map(x=>caches.delete(x)))));
+});
+self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request))});
