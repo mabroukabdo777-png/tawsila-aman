a78d90a197f78a156bf674b65421f5abb1a21325
mabroukabdo777-png <mabroukabdo777@gmail.com>
2026-09-12 04:30:22 +0300

    Delete sw.js

--- sw.js
+++
@@ -1,7 +0,0 @@
-const VERSION='v4-black-premium';
-self.addEventListener('install',e=>{self.skipWaiting()});
-self.addEventListener('activate',e=>{
-  e.waitUntil(clients.claim());
-  e.waitUntil(caches.keys().then(k=>Promise.all(k.map(x=>caches.delete(x)))));
-});
-self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request))});
