6e1199f0006ecfb0998f23caa7f45f0e9b523d65
mabroukabdo777-png <mabroukabdo777@gmail.com>
2026-09-12 11:08:30 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,11 @@
-const CACHE='matam-masr-v16-luxury';
-self.addEventListener('install',e=>{
-  self.skipWaiting();
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json'])));
-});
-self.addEventListener('activate',e=>{
-  e.waitUntil(
-    caches.keys().then(keys=>Promise.all(keys.map(k=>{
-      if(k!==CACHE) return caches.delete(k);
-    }))).then(()=>self.clients.claim())
-  );
-});
+const CACHE='matam-masr-v17-luxury-auto';
+self.addEventListener('install',e=>{ self.skipWaiting(); e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json']))); });
+self.addEventListener('activate',e=>{ e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>{ if(k!==CACHE) return caches.delete(k); }))).then(()=>self.clients.claim())); });
 self.addEventListener('fetch',e=>{
-  e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
+  if(e.request.mode==='navigate' || e.request.url.includes('index.html')){
+    e.respondWith(fetch(e.request).then(r=>{ const cl=r.clone(); caches.open(CACHE).then(c=>c.put(e.request,cl)); return r; }).catch(()=>caches.match(e.request)));
+  } else {
+    e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request)));
+  }
 });
+self.addEventListener('message',e=>{ if(e.data==='SKIP_WAITING') self.skipWaiting(); });
