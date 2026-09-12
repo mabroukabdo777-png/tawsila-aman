5900335ded3632bdadd531bbc373a326db3bec0f
mabroukabdo777-png <mabroukabdo777@gmail.com>
2026-09-12 05:40:27 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,3 @@
+const CACHE='tawsil-aman-v5';
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json'])));});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
