eaf053990c90b26ec559105322c4aad7e9552348
mabroukabdo777-png <mabroukabdo777@gmail.com>
2026-09-12 10:40:13 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,15 @@
-const CACHE='tawsil-aman-v5';
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json'])));});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
+const CACHE='matam-masr-v16-luxury';
+self.addEventListener('install',e=>{
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json'])));
+});
+self.addEventListener('activate',e=>{
+  e.waitUntil(
+    caches.keys().then(keys=>Promise.all(keys.map(k=>{
+      if(k!==CACHE) return caches.delete(k);
+    }))).then(()=>self.clients.claim())
+  );
+});
+self.addEventListener('fetch',e=>{
+  e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
+});
