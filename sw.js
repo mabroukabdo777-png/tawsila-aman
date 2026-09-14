91ff0eaae667e184b5f84396aef1c294303acd41
mabroukabdo777-png <mabroukabdo777@gmail.com>
2026-09-14 04:04:21 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,4 @@
-const CACHE_NAME="tawsila-v7";
-self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(["/","/logo.png?v=7"])))});
+const CACHE_NAME="tawsila-v10";
+self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(["/","/index.html?v=10","/logo.png?v=10","/manifest.json?v=10"])))});
+self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>{if(k!==CACHE_NAME)return caches.delete(k)}))))});
 self.addEventListener("fetch",e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
