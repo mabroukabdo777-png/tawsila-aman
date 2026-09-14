9ba397f7f9eb7bab86b9a3b9b5ca6032a1ea29d5
mabroukabdo777-png <mabroukabdo777@gmail.com>
2026-09-14 03:51:21 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,3 @@
-self.addEventListener('install', (e) => {
-  self.skipWaiting();
-});
-self.addEventListener('activate', (e) => {
-  e.waitUntil(clients.claim());
-});
-self.addEventListener('fetch', (e) => {
-  e.respondWith(fetch(e.request).catch(()=> caches.match(e.request)));
-});
+const CACHE_NAME="tawsila-v7";
+self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(["/","/logo.png?v=7"])))});
+self.addEventListener("fetch",e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
