bf59593d3ef3ccfc1a81c38796a58aa0536670d6
mabroukabdo777-png <mabroukabdo777@gmail.com>
2026-09-18 00:17:07 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,12 @@
-const CACHE_NAME = 'mot3amy-v2'; // غير الرقم ده كل ما تعمل تحديث
-
-self.addEventListener('install', (e) => {
-  self.skipWaiting(); // يخليه يحدث فوراً
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(['./','./index.html','./logo.png','./icon-192.png']))
-  );
+const CACHE_NAME = 'mot3amy-v5-337-real';
+const ASSETS = ['./','./index.html','./manifest.json'];
+self.addEventListener('install', e=>{
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)));
 });
-
-self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(k => { if(k !== CACHE_NAME) return caches.delete(k) }))).then(()=>self.clients.claim())
-  );
+self.addEventListener('activate', e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>{ if(k!==CACHE_NAME) return caches.delete(k); }))).then(()=>self.clients.claim()));
 });
-
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    fetch(e.request).catch(()=>caches.match(e.request))
-  );
+self.addEventListener('fetch', e=>{
+  e.respondWith(fetch(e.request).then(r=>r).catch(()=>caches.match(e.request)));
 });
