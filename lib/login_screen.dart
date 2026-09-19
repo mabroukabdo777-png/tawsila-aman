f0a0209c53aac885f2c0e28572a1c92e6428ca6a
mabroukabdo777-png <mabroukabdo777@gmail.com>
2026-09-19 15:29:19 +0300

    Create login_screen.dart

---
+++ lib/login_screen.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart'; 
+class LoginScreen extends StatelessWidget { 
+@override Widget build(BuildContext c){ 
+return Scaffold(body: Center(child: Text('كابتن فلاش - تسجيل دخول', style: TextStyle(fontSize: 22)))); 
+} }
