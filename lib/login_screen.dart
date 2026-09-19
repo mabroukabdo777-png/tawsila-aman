a04ca11c2a260f6866ca4bbb7e8668d56e27bc27
mabroukabdo777-png <mabroukabdo777@gmail.com>
2026-09-19 20:58:08 +0300

    Delete lib directory

--- lib/login_screen.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/material.dart'; 
-class LoginScreen extends StatelessWidget { 
-@override Widget build(BuildContext c){ 
-return Scaffold(body: Center(child: Text('كابتن فلاش - تسجيل دخول', style: TextStyle(fontSize: 22)))); 
-} }
--- lib/services/firebase_service.dart
+++
@@ -1,2 +0,0 @@
-import 'package:firebase_database/firebase_database.dart';
-class FirebaseService { final db = FirebaseDatabase.instanceFor(app: null, databaseURL: 'https://soqshpin-default-rtdb.firebaseio.com').ref(); }
