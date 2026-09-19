0cea099b3c6df901c79c560a97f48e2b25f8c68f
mabroukabdo777-png <mabroukabdo777@gmail.com>
2026-09-19 15:54:47 +0300

    Create delivery_home.dart

---
+++ lib/screens/delivery_home.dart
@@ -0,0 +1,2 @@
+import 'package:flutter/material.dart';
+class DeliveryHome extends StatelessWidget { @override Widget build(BuildContext c) => Scaffold(appBar: AppBar(title: Text('مندوب توصيل')), body: Center(child: Text('مرحبا مندوب'))); }
