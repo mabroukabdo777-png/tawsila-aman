8dcb5aa020de3376999389d79d01fb445438dfb5
mabroukabdo777-png <mabroukabdo777@gmail.com>
2026-09-19 20:55:39 +0300

    Delete lib/screens directory

--- lib/screens/delivery_home.dart
+++
@@ -1,2 +0,0 @@
-import 'package:flutter/material.dart';
-class DeliveryHome extends StatelessWidget { @override Widget build(BuildContext c) => Scaffold(appBar: AppBar(title: Text('مندوب توصيل')), body: Center(child: Text('مرحبا مندوب'))); }
--- lib/screens/map_screen.dart
+++
@@ -1,2 +0,0 @@
-import 'package:flutter/material.dart';
-class MapScreen extends StatelessWidget { @override Widget build(BuildContext c) => Scaffold(body: Center(child: Text('الخريطة'))); }
